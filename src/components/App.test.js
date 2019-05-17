da99cb475e99ff999c8f46f418fa78b9a3c12d96
simotae14 <startae14@gmail.com>
2019-05-17 22:52:38 +0200

    created the first test, the component and the generated snapshot

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { shallow, configure } from 'enzyme';
+import toJson from 'enzyme-to-json';
 import App from './App';
+
+configure({adapter: new Adapter()});
 
 // create the shallow rendering
 const app = shallow(<App />);
 
 // 1st unit test, renders correctly App
 it('renders correctly', () => {
-    expect(app).toMatchSnapshot();
+    expect(toJson(app)).toMatchSnapshot();
 });
