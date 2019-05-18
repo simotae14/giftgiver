08fa365af4f8df066f5c5576a108844c91206deb
Simone Taeggi <startae14@gmail.com>
2019-05-18 11:14:41 +0200

    create the state inside Gift to pass the test

--- src/components/Gift.js
+++ src/components/Gift.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
 
 class Gift extends Component {
+    constructor() {
+        super();
+        this.state = {
+            person: '',
+            present: ''
+        };
+    }
     render() {
         return(
             <div></div>
