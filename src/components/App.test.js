5279b040fa06dcb65e7b577f47671d4b39caba66
Simone Taeggi <startae14@gmail.com>
2019-05-18 10:56:41 +0200

    Refactor tests with a BDD approach

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -6,35 +6,47 @@ import App from './App';
 
 configure({adapter: new Adapter()});
 
-// create the shallow rendering
-const app = shallow(<App />);
+// create the group of tests
+describe('App', () => {
+    // create the shallow rendering
+    const app = shallow(<App />);
 
-// 1st unit test, renders correctly App
-it('renders correctly', () => {
-    expect(toJson(app)).toMatchSnapshot();
-});
+    // 1st unit test, renders correctly App
+    it('renders correctly', () => {
+        expect(toJson(app)).toMatchSnapshot();
+    });
 
-// 2nd unit test, initialize component state with an empty list of gifts
-it('initializes the `state` with an empty list of gifts', () => {
-    expect(app.state().gifts).toEqual([]);
-});
+    // 2nd unit test, initialize component state with an empty list of gifts
+    it('initializes the `state` with an empty list of gifts', () => {
+        expect(app.state().gifts).toEqual([]);
+    });
 
-// update the state when we click the add gift button
-it('adds a new gift to `state` when clicking the `add gift` button', () => {
-    // find the button and click it
-    app.find('.btn-add').simulate('click');
+    // scenario: Click the add click button
+    describe('when clicking the `add-gift` button', () => {
+        beforeEach(() => {
+            // find the button and click it
+            app.find('.btn-add').simulate('click');
+        });
+        // reset the state to the default value
+        afterEach(() => {
+            // reset the state
+            app.setState({
+                gifts: []
+            });
+        });
+        // update the state when we click the add gift button
+        it('adds a new gift to `state`', () => {
+            // add a gift into the state
+            expect(app.state().gifts).toEqual([{
+                id: 1
+            }]);
+        });
 
-    // add a gift into the state
-    expect(app.state().gifts).toEqual([{
-        id: 1
-    }]);
-});
+        // render the new gift in the list of gifts
+        it('adds a new gift to the rendered list', () => {
+            // check if the gift list children are 2
+            expect(app.find('.gift-list').children().length).toEqual(1);
+        });
+    });
 
-// render the new gift in the list of gifts
-it('adds a new gift to the rendered list when clicking the `add gift` button', () => {
-    // simulate click on the button
-    app.find('.btn-add').simulate('click');
-
-    // check if the gift list children are 2
-    expect(app.find('.gift-list').children().length).toEqual(2);
 });
