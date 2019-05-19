dd015c5fd1af7b9e7334a9e26466eb0494240a07
Simone Taeggi <startae14@gmail.com>
2019-05-19 12:26:06 +0200

    Test Coverage and Test and Implement an helper method

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Button } from 'react-bootstrap';
 import Gift from './Gift';
+import { max_number } from '../helper';
+
 class App extends Component {
     constructor() {
         super();
@@ -14,7 +16,7 @@ class App extends Component {
         // create the list of ids
         const ids = this.state.gifts.map(gift => gift.id);
         // find the max ID
-        const max_id = ids.length > 0 ? Math.max(...ids) : 0;
+        const max_id = max_number(ids);
 
         gifts.push({
             id: max_id + 1
